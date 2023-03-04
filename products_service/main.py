94ec9f7908e3f099b03507d86a74c00845538501
NogueiraDan <silvanogueira3460@gmail.com>
2023-03-04 17:28:12 -0300

    Alteração no serviço de produto

--- products_service/main.py
+++ products_service/main.py
@@ -8,6 +8,7 @@ async def read_products():
         { "id": 1, "name": "Smartphone" },
         { "id": 2, "name": "SmartTV" },
         { "id": 3, "name": "Notebook" },
+        { "id": 4, "name": "Tablet" },
     ]
 
     return products
